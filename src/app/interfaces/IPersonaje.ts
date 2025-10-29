87889bf42a7302f4cca31b7ed49d0a6145d9279b
Eva F.M. <eafernandez@educarex.es>
2025-10-29 14:21:07 +0100

    botones

--- src/app/interfaces/IPersonaje.ts
+++ src/app/interfaces/IPersonaje.ts
@@ -1,15 +1,17 @@
-export enum TIPO_PESONAJE{
-    mago = 'Mago',
-    guerrero = 'Guerrero',
-    arquero = 'Arquero',
-    druida = 'Druida'
+export const TOPE_VIDA: number = 100;
+
+export enum TIPO_PESONAJE {
+  mago = 'Mago',
+  guerrero = 'Guerrero',
+  arquero = 'Arquero',
+  druida = 'Druida',
 }
 
-export interface IPersonaje{
-    nombre: string;
-    nivel: number;
-    tipo: TIPO_PESONAJE;
-    habilidad?: string[];
-    vida: number;
-    imagen: string;
+export interface IPersonaje {
+  nombre: string;
+  nivel: number;
+  tipo: TIPO_PESONAJE;
+  habilidad?: string[];
+  vida: number;
+  imagen: string;
 }
--- src/app/pages/detalle-personaje/detalle-personaje.ts
+++ src/app/pages/detalle-personaje/detalle-personaje.ts
@@ -1,24 +1,36 @@
 import { Component } from '@angular/core';
-import { IPersonaje, TIPO_PESONAJE } from '../../interfaces/IPersonaje';
+import { IPersonaje, TIPO_PESONAJE, TOPE_VIDA } from '../../interfaces/IPersonaje';
 
 @Component({
   selector: 'app-detalle-personaje',
   imports: [],
   templateUrl: './detalle-personaje.html',
-  styleUrl: './detalle-personaje.css'
+  styleUrl: './detalle-personaje.css',
 })
 export class DetallePersonaje {
-  
   personaje: IPersonaje = {
     nombre: 'Gandalf',
     nivel: 70,
     tipo: TIPO_PESONAJE.mago,
     vida: 0,
-    imagen: './gandalf.png'
+    imagen: './gandalf.png',
+  };
+
+  constructor() {}
+
+  resucitar(): void {
+    this.personaje.vida = TOPE_VIDA;
   }
 
-  constructor (){
+  matar(): void {
+    this.personaje.vida = 0;
   }
 
-
+  hacerPupa (): void{
+    const pupa = Math.round(Math.random()*(TOPE_VIDA/2));
+    if (this.personaje.vida - pupa < 0)
+      this.matar();
+    else
+      this.personaje.vida-=pupa
+  } 
 }
