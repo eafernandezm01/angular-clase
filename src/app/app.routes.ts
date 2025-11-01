6d15a8fad2576cebb9f68fe49332e00642cea101
Eva F.M. <eafernandez@educarex.es>
2025-11-01 18:40:49 +0100

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,7 @@ import { Routes } from '@angular/router';
 import { DetallePersonaje } from './pages/detalle-personaje/detalle-personaje';
 
 export const routes: Routes = [
-    { path: 'detalle-personaje', component: DetallePersonaje}
+  { path: 'detalle-personaje', component: DetallePersonaje },
+  // ruta por defecto cuando ponga la / que muestre detalle
+  { path: '', redirectTo: 'detalle-personaje', pathMatch: 'full' },
 ];
