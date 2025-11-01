538b4d27880252ebbe00baca4cd4566f5a1287d3
Eva F.M. <eafernandez@educarex.es>
2025-11-01 18:36:50 +0100

    .

--- src/app/pages/detalle-personaje/detalle-personaje.ts
+++ src/app/pages/detalle-personaje/detalle-personaje.ts
@@ -8,6 +8,9 @@ import { IPersonaje, TIPO_PESONAJE, TOPE_VIDA } from '../../interfaces/IPersonaj
   styleUrl: './detalle-personaje.css',
 })
 export class DetallePersonaje {
+
+  topeVida:number = TOPE_VIDA;
+
   personaje: IPersonaje = {
     nombre: 'Gandalf',
     nivel: 70,
@@ -26,11 +29,27 @@ export class DetallePersonaje {
     this.personaje.vida = 0;
   }
 
-  hacerPupa (): void{
-    const pupa = Math.round(Math.random()*(TOPE_VIDA/2));
-    if (this.personaje.vida - pupa < 0)
-      this.matar();
-    else
-      this.personaje.vida-=pupa
-  } 
+  hacerPupa(): void {
+    const pupa = Math.round(Math.random() * (TOPE_VIDA / 2));
+    if (this.personaje.vida - pupa < 0) this.matar();
+    else this.personaje.vida -= pupa;
+  }
+
+
+  getClassVida(): string {
+    const { vida } = this.personaje;
+
+    if (vida === 0) return 'rojo';
+    if (vida > 0 && vida <= 25) return 'naranja';
+    if (vida > 25 && vida <= 50) return 'amarillo';
+    if (vida > 50 && vida <= 75) return 'verde-claro';
+    return 'verde-oscuro';
+  }
+
+  sanar (): void {
+    const cura = Math.round(Math.random() * (TOPE_VIDA / 2));
+    if (this.personaje.vida + cura <  TOPE_VIDA)
+        this.personaje.vida += cura;
+    else this.resucitar();
+  }
 }
