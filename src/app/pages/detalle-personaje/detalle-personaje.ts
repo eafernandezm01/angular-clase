9438b9541291a9f92e0c752c9232648bf945feed
Eva F.M. <eafernandez@educarex.es>
2025-11-03 16:20:25 +0100

    input

--- src/app/pages/detalle-personaje/detalle-personaje.ts
+++ src/app/pages/detalle-personaje/detalle-personaje.ts
@@ -1,55 +1,28 @@
 import { Component } from '@angular/core';
-import { IPersonaje, TIPO_PESONAJE, TOPE_VIDA } from '../../interfaces/IPersonaje';
+import { Personaje } from '../../personaje/personaje/personaje';
+import { IPersonaje, TIPO_PESONAJE } from '../../interfaces/IPersonaje';
 
 @Component({
   selector: 'app-detalle-personaje',
-  imports: [],
+  imports: [Personaje],
   templateUrl: './detalle-personaje.html',
   styleUrl: './detalle-personaje.css',
 })
 export class DetallePersonaje {
 
-  topeVida:number = TOPE_VIDA;
+    gandalf: IPersonaje = {
+      nombre: 'Gandalf',
+      nivel: 70,
+      tipo: TIPO_PESONAJE.mago,
+      vida: 0,
+      imagen: './gandalf.png',
+    };
 
-  personaje: IPersonaje = {
-    nombre: 'Gandalf',
-    nivel: 70,
-    tipo: TIPO_PESONAJE.mago,
-    vida: 0,
-    imagen: './gandalf.png',
-  };
-
-  constructor() {}
-
-  resucitar(): void {
-    this.personaje.vida = TOPE_VIDA;
-  }
-
-  matar(): void {
-    this.personaje.vida = 0;
-  }
-
-  hacerPupa(): void {
-    const pupa = Math.round(Math.random() * (TOPE_VIDA / 2));
-    if (this.personaje.vida - pupa < 0) this.matar();
-    else this.personaje.vida -= pupa;
-  }
-
-
-  getClassVida(): string {
-    const { vida } = this.personaje;
-
-    if (vida === 0) return 'rojo';
-    if (vida > 0 && vida <= 25) return 'naranja';
-    if (vida > 25 && vida <= 50) return 'amarillo';
-    if (vida > 50 && vida <= 75) return 'verde-claro';
-    return 'verde-oscuro';
-  }
-
-  sanar (): void {
-    const cura = Math.round(Math.random() * (TOPE_VIDA / 2));
-    if (this.personaje.vida + cura <  TOPE_VIDA)
-        this.personaje.vida += cura;
-    else this.resucitar();
-  }
+    ladybug: IPersonaje = {
+      nombre: 'LadyBug',
+      nivel: 40,
+      tipo: TIPO_PESONAJE.druida,
+      vida: 100,
+      imagen: './gandalf.png',
+    };
 }
